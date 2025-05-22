289259f262e4edddd730a1b0054888dc1178e61b
Nils Hörrmann <nils.hoerrmann@hananils.de>
2025-05-22 08:44:28 +0200

    Composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hananils/kirby-checkbox-field',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cd6963bb671e27d5ffe5fd3b9edc52b766e25c8a',
+        'type' => 'kirby-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'hananils/kirby-checkbox-field' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cd6963bb671e27d5ffe5fd3b9edc52b766e25c8a',
+            'type' => 'kirby-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
